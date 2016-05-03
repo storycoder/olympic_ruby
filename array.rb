910dd134674cc840635e4b0909fda5dfd735e203
Storycoder <joseph.margolis@gmail.com>
2016-05-03 10:26:34 -0400

    array

---
+++ array.rb
@@ -0,0 +1,10 @@
+#An array of three strings and puts statement to access one of the values
+my_array = ["rock", "paper", "scissors"]
+#The next line accesses "rock" in a puts statement
+puts my_array[0]
+#The next line accesses "paper" in a puts statement
+puts my_array[1]
+#The next line accesses "scissors" in a puts statement
+puts my_array[2]
+#The next line accesses my_array in a puts statement
+puts my_array
